8ca5d5995be62eecbbc6f8d7a751b54885ac5487
Gabriella Hon <gabriella.hon@students.wits.ac.za>
2025-04-07 14:27:42 +0200

    code_style_and_Second_fix_attempt

--- code/getbest.py
+++ code/getbest.py
@@ -9,21 +9,23 @@ def getCols(f):
     for head in headings:
         if head == "Student Number": num_col = i
         elif head == "Mark" : mark_col = i
-    return (num_col, mark_col)
+    return num_col, mark_col
 
 ''' Finds the top student in the class '''
+# Assuming all marks are distinct
 def findTop(f,num_col, mark_col):
     best = best_idx =  0
     for line in f:
         data = line.strip().split(",")
         mark = int(data[mark_col])
         if mark > best:
-            best = mark
+          best = mark
+          best_idx += 1
     return best_idx, best
 
-
+# Run the test
 if __name__ == '__main__':
     f = open(sys.argv[1])
     num_col, mark_col = getCols(f)
     best_idx, best = findTop(f,num_col,mark_col)
-    print("The top student was student %s with %d"%(best,best_idx))
+    print("The top student was student %s with %d"%(best, best_idx))
